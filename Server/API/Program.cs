eee87f64867b13fae1e24a6a02f23e6992d0d371
= <=>
2025-10-05 16:33:15 +0300

    init

---
+++ Server/API/Program.cs
@@ -0,0 +1,28 @@
+namespace API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            Console.WriteLine(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
